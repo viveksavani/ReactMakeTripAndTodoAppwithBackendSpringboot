4c37350713509aea8ca9be1a9712c2b0579e0384
savani vivek <savani.vivek3@gmail.com>
2023-05-11 22:35:37 +0530

    crud operation added

--- todo-app/src/components/todo/HeaderComponent.jsx
+++ todo-app/src/components/todo/HeaderComponent.jsx
@@ -9,7 +9,7 @@ function HeaderComponent() {
     return (
         <header>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                <div><a href="http://www.in28minutes.com" className="navbar-brand">in28Minutes</a></div>
+                <div><a href="" className="navbar-brand">Todo APP</a></div>
                 <ul className="navbar-nav">
                     {userLoggedIn && <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>}
                     {userLoggedIn && <li><Link className="nav-link" to="/todos">Todos</Link></li>}
