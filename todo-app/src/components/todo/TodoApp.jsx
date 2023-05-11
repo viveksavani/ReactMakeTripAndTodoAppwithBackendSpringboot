82959ffe21dcc7211e554246a4dfefe4b4dd39c1
savani vivek <savani.vivek3@gmail.com>
2023-05-11 22:51:13 +0530

    login and logout added with authentication

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter, Routes} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginComponent from "./LoginComponent.jsx";
 import ListTodosComponent from "./ListTodosComponent.jsx";
@@ -9,66 +9,60 @@ import HeaderComponent from "./HeaderComponent.jsx";
 import FooterComponent from "./FooterComponent.jsx";
 import LogoutComponent from "./LogoutComponent.jsx";
 import TodoComponent from "./TodoComponent.jsx";
+import withNavigation from "./WithNavigation.jsx"
+import withParams from "./WithParams.jsx"
+import AuthenticatedRoute from "./AuthenticatedRoute.jsx"
 
 class TodoApp extends Component {
-
-
     render() {
+    	const LoginComponentWithNavigation = withNavigation(LoginComponent);
+
+    	const WelcomeComponentWithParams = withParams(WelcomeComponent);
+
+    	const HeaderComponentWithNavigation = withNavigation(HeaderComponent);
+
+    	const TodoComponentWithParamsAndNavigation = withParams(withNavigation(TodoComponent));
+
+    	const ListTodosComponentWithNavigation = withNavigation(ListTodosComponent) //REACT-6
 
         return (
-            <BrowserRouter>
-                <div className="TodoApp">
-                <HeaderComponent/>
-                    <Routes>
-                        <>
-                        <Route path="/" exact element={<LoginComponent />} />
-                        <Route path="/login" element={<LoginComponent />} />
-                        <Route path='/welcome/:name' element={<WelcomeComponent />} />
-                        <Route path='/todos' element={<ListTodosComponent />} />
-                        <Route path='/logout' element={<LogoutComponent />} />
-                        <Route path='/todos/:id' element={<TodoComponent />} />
-                        <Route path="*" element={<ErrorComponent />} />
-                        </>
-                    </Routes>
-                    <FooterComponent/>
+            <div className="TodoApp">
+            	<Router>
+            		<HeaderComponentWithNavigation/>
+            		<Routes>
+		            	<Route path="/" element={<LoginComponentWithNavigation />} />
+		            	<Route path="/login" element={<LoginComponentWithNavigation />} />
+		            	<Route path="/welcome/:name" element={
+		            		<AuthenticatedRoute>
+		            			<WelcomeComponentWithParams />
+		            		</AuthenticatedRoute>
+		            	} />
+		            	
+		            	//REACT-6
+		            	<Route path="/todos/:id" element={ 
+			            	<AuthenticatedRoute>
+			            		<TodoComponentWithParamsAndNavigation />
+			            	</AuthenticatedRoute>
+			            } />
 
-                    {/*  <LoginComponent />
-                 <WelcomeComponent /> */}
-                </div>
-            </BrowserRouter>
-
+			            //REACT-6
+		            	<Route path="/todos" element={
+			            	<AuthenticatedRoute>
+			            		<ListTodosComponentWithNavigation /> 
+			            	</AuthenticatedRoute>
+			            } />
+		            	<Route path="/logout" element={
+		            		<AuthenticatedRoute>
+		            			<LogoutComponent />
+		            		</AuthenticatedRoute>
+		            	} />
+		            	<Route path="*" element={<ErrorComponent />} />
+	            	</Routes>
+	            	<FooterComponent/>
+            	</Router>
+            </div>
         )
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function ShowInvalidCredentials(props){
-
-//     if(props.hasLoginFailed){
-//         return <div>Invalid Credentials</div>
-//     }
-//     return null
-// }
-
-// function ShowSuccessCredentials(props){
-
-//     if(props.showSuccessMessage){
-//         return  <div>Login Succesfull</div>
-//     }
-//     return null
-// }
-
-export default TodoApp;
+export default TodoApp
